9918267fbe222c959c461818ba6ca8e73e48d777
Imran Zukorlic <mr.silver020@gmail.com>
2022-09-21 19:06:42 +0200

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1 className="text-3xl font-bold underline text-indigo-400">
+          Hello world!
+        </h1>
+      </div>
+      <div className=" flex justify-between flex-row w-1/2 mx-auto items-centar h-24 bg-slate-400 mt-20 rounded-xl px-8 ">
+        <div className="flex  justify-center items-center bg-blue-500 w-16 h-16 text-white font-bold underline rounded-full ">
+          prvi
+        </div>
+        <div className="flex  justify-center items-center bg-orange-500 w-16 h-16 text-white font-bold underline rounded-xl ">
+          prvi
+        </div>
+        <div className="flex  justify-center items-center bg-red-500 w-16 h-16 text-white font-bold underline rounded-lg ">
+          prvi
+        </div>
+        {/* <button className="bg-blue-500 hover:bg-blue-50 transition text-white font-bold py-4  px-10 rounded-xl">
+          Sing In
+        </button> */}
+      </div>
+    </>
   );
 }
 
