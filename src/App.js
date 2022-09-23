2372e676beac6f5135b3c459506c7b9d6cf3c02e
Imran Zukorlic <mr.silver020@gmail.com>
2022-09-23 19:00:38 +0200

    cart

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+// import "./App.css";
+// import Card from "./components/Card/Card";
+import NavBar from "./components/Navigation/Nav";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <h1 className="text-3xl font-bold underline text-indigo-400">
-          Hello world!
-        </h1>
-      </div>
-      <div className=" flex justify-between flex-row w-1/2 mx-auto items-centar h-24 bg-slate-400 mt-20 rounded-xl px-8 ">
-        <div className="flex  justify-center items-center bg-blue-500 w-16 h-16 text-white font-bold underline rounded-full ">
-          prvi
-        </div>
-        <div className="flex  justify-center items-center bg-orange-500 w-16 h-16 text-white font-bold underline rounded-xl ">
-          prvi
-        </div>
-        <div className="flex  justify-center items-center bg-red-500 w-16 h-16 text-white font-bold underline rounded-lg ">
-          prvi
-        </div>
-        {/* <button className="bg-blue-500 hover:bg-blue-50 transition text-white font-bold py-4  px-10 rounded-xl">
-          Sing In
-        </button> */}
-      </div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<>home</>} />
+        <Route path="/about" element={<>about</>} />
+        <Route path="/cart" element={<>cart</>} />
+      </Routes>
     </>
   );
 }
